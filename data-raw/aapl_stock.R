b272955915ed03223fb57fcd634ae0f7e46da2d5
trevorkwan <54831831+trevorkwan@users.noreply.github.com>
2020-11-17 20:22:35 -0800

    Update aapl_stock.R

--- data-raw/aapl_stock.R
+++ data-raw/aapl_stock.R
@@ -1,3 +1,4 @@
+# load required packages
 if (!require("quantmod")) {
   install.packages("quantmod")
   library(quantmod)
@@ -8,9 +9,12 @@ if (!require("magrittr")) {
   library("magrittr")
 }
 
+# get aapl stock data from yahoo finance source
 aapl_stock <- getSymbols("AAPL", src = "yahoo", auto.assign = FALSE)
 
+# convert data object into dataframe
 aapl_stock <- as.data.frame(aapl_stock)
 
+# save dataframe object as .rda and .rds in test folder and data folder
 save(aapl_stock, file = "data/aapl_stock.rda", compress='bzip2')
 saveRDS(aapl_stock, "tests/testthat/aapl_stock.rds")
