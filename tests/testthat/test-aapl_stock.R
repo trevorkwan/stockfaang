ecbea02672e086a837128a945b283ea299ef901f
trevorkwan <54831831+trevorkwan@users.noreply.github.com>
2020-11-17 20:34:32 -0800

    Update test-aapl_stock.R

--- tests/testthat/test-aapl_stock.R
+++ tests/testthat/test-aapl_stock.R
@@ -1,3 +1,4 @@
+# test to see that the generated aapl_stock dataframe is the same as the aapl_stock.rds file in this folder
 test_that("aapl_stock has not changed", {
   expect_equal_to_reference(aapl_stock, "aapl_stock.rds")
 })
